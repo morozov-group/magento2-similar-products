122c09d3b76d2261d99baf49e1189457bfe26578
anatol1984 <anatol.kravchenko@gmail.com>
2018-10-23 21:55:10 +0300

    [SIM-1] extension dummy

---
+++ app/code/Morozov/Similarity/Controller/Test/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace Morozov\Similarity\Controller\Test;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+
+class Index extends Action
+{
+    public function __construct(
+        Context $context
+    ) {
+        parent::__construct(
+            $context
+        );
+
+       
+    }
+
+    public function execute()
+    {
+
+    }
+}
