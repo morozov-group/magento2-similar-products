19fb00857e2f296808a27fe83bf6b26c29ab3b6a
anatol1984 <anatol.kravchenko@gmail.com>
2018-10-26 21:22:41 +0300

    [SIM-1] dummy data for plugin

--- Plugin/Catalog/Model/Product.php
+++ Plugin/Catalog/Model/Product.php
@@ -3,12 +3,33 @@ namespace Morozov\Similarity\Plugin\Catalog\Model;
 
 class Product
 {
+    protected $collectionFactory;
+
+    protected $defaultHelper;
+
+    public function __construct(
+        \Magento\Catalog\Model\ResourceModel\Product\Link\Product\CollectionFactory $collectionFactory,
+        \Morozov\Similarity\Helper\Data $defaultHelper
+    ) {
+        $this->collectionFactory = $collectionFactory;
+        $this->defaultHelper = $defaultHelper;
+    }
+
     public function afterGetUpSellProductCollection(
         \Magento\Catalog\Model\Product $product,
         $upSellProductsCollection
     )
     {
-        //@TODO: pull upsells from the service
+        if ($this->defaultHelper->canUse()) {
+            try {
+                //@TODO: pull upsells from the service
+                $productCollection = $this->collectionFactory->create();
+                $productCollection->addAttributeToFilter('entity_id', ['in' => [7, 8]]);
+                return $productCollection;
+            } catch (\Exception $e) {
+                $this->defaultHelper->log($e->getMessage());
+            }
+        }
 
         return $upSellProductsCollection;
     }
