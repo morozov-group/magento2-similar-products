eb3e41be0f69aadeaabd7d6d62a06f53e62eb63e
Timur Ganeev <ggtimur@gmail.com>
2019-02-08 17:12:42 +0300

    fix reindex and add widget

--- Helper/Product.php
+++ Helper/Product.php
@@ -52,7 +52,7 @@ class Product extends \Magento\Framework\App\Helper\AbstractHelper
     {
         $csvDir = $this->similarityHelper->getExportDir();
         $pubMediaDir = $this->directoryList->getPath(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA);
-        $mediaUrl = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+        $mediaUrl = $this->storeManager->getStore()->getBaseUrl() . 'media';
 
         if (!is_dir($csvDir)) {
             if (!mkdir($csvDir)) {
